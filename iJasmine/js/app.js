42f8d68f6a80eb684aa12ba9552667efd83922e6
mayubo <mayubo@haizhi.com>
2017-11-05 15:16:13 +0800

    destination.html

--- iJasmine/js/app.js
+++ iJasmine/js/app.js
@@ -2,7 +2,9 @@ var iJasmineApp = angular.module('iJasmine', []);
 
 iJasmineApp.controller('iJasmineCtrl', function($scope,$http) {
 
+
     $scope.viewData = {
+        view: 'home',
         geoData: null,
         geoInfo: {
             geoKey: "New Zealand",
@@ -22,13 +24,30 @@ iJasmineApp.controller('iJasmineCtrl', function($scope,$http) {
 
     $scope.$on('emitUpdateGeoInfo',function(e, data){
         $scope.viewData.geoInfo = data;
-        console.log($scope.viewData)
         $scope.$apply();
     });
 
     $http.get('src/jsonData/geoData.json').then(function (result) {
         $scope.viewData.geoData = result.data;
     });
+
+    if( $("#scroll_banner").scrollBanner ){
+        $("#scroll_banner").scrollBanner({
+            images : [
+                {src:"src/imgs/6.jpg",title:"banner1"},
+                {src:"src/imgs/7.jpg",title:"banner2"},
+                {src:"src/imgs/8.jpg",title:"banner3"},
+                {src:"src/imgs/9.jpg",title:"banner4"}
+            ],
+            scrollTime: 4000,
+            bannerWidth:"1080px",
+            bannerHeight:"500px",
+            iconColor: "#FFFFFF",
+            iconHoverColor : "#f76459",
+            iconPosition : "center"
+        });
+    }
+
 
 
 });
