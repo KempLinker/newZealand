f679801f9ff8eb434d4cdd4ba7d9b32728f98bbc
mayubo <mayubo@haizhi.com>
2018-01-09 23:53:23 +0800

    问卷页面展示

--- iJasmine/js/controllers/questionnaire-ctrl.js
+++ iJasmine/js/controllers/questionnaire-ctrl.js
@@ -7,9 +7,18 @@
     function questionnaireCtrl($scope,$http) {
 
         $scope.viewData = {
-
+            init: false,
+            list: []
         };
 
+        $http.get(__Public+'/jsonData/questionnaireData.json').then(function (result) {
+            handleData(result.data);
+            $scope.viewData.init = true;
+        });
+
+        function handleData( data ){
+            $scope.viewData.list = data.questionnaireList;
+        }
 
     }
 })();
