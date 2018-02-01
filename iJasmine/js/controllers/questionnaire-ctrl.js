21d353e67658f363f88b9923e8f40a1a3d31b3a3
mayubo <mayubo@haizhi.com>
2018-02-01 23:37:05 +0800

    问卷调查存数据v1

--- iJasmine/js/controllers/questionnaire-ctrl.js
+++ iJasmine/js/controllers/questionnaire-ctrl.js
@@ -22,7 +22,42 @@
         }
 
         function submitFunc() {
-            console.log($scope.viewData.list);
+            var list = $scope.viewData.list;
+            var validFlag = true;
+            var finalObj = {
+                user: {},
+                number: {},
+                time: {},
+                nature: {},
+            };
+            // 联系人
+            var user = list['user'][0];
+            user.sub.forEach(function(item){
+                finalObj['user'][item.key] = item.val
+            });
+            // 出行人数
+            var number = list['numberOfPeople'][0];
+            finalObj['number'].val = number.val;
+            finalObj['number'].detail = [];
+            number.sub.forEach(function(item){
+                finalObj['number'].detail.push(item.val || -1);
+            });
+            // 旅行天数
+            var time = list['travelTime'][0];
+            finalObj['time'].type = time.val;
+            finalObj['time'].val = time.sub[0].val;
+            finalObj['time'].detail = [];
+            time.sub[0].sub.forEach(function(item){
+                if( item.val == time.val ){
+                    item.sub.forEach(function(i){
+                        finalObj['time'].detail.push(i.val || -1);
+                    });
+                }
+            });
+            // 旅行性质
+            var nature = list['travelNature'][0];
+            console.log(nature);
+            console.log(finalObj);
         }
 
     }
